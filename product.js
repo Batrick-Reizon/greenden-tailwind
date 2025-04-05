3dd4ae2ef1c1411c924ba6a646c36013827595e6
Batrick-Reizon <steynd21@gmail.com>
2025-04-05 10:58:04 +0530

    updated by reizon

--- product.js
+++ product.js
@@ -13,10 +13,10 @@ cancel.addEventListener("click",function(){
 var searchbox=document.getElementById("searchbox")
 var products=document.querySelectorAll(".m-2")
 
-searchbox.addEventListener("keyup",function(){
+searchbox.addEventListener("keyup",function(event){
     var enteredvalue=event.target.value.toUpperCase()
     
-    for(i=0;i<products.length;i++){
+    for(let i=0;i<products.length;i++){
         var h1list=products[i].querySelector("h1").textContent.toUpperCase()
         if(h1list.indexOf(enteredvalue)<0){
             products[i].style.display="none"
